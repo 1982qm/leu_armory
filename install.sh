9bab0a94fe837ac6d7d64be0ebac27538f2fbda8
Energy <1982.qm@gmail.com>
2025-12-24 12:02:02 +0100

    Aggiunto .gitignore

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-rm -rf /var/www/html/leu/css/*
-rm -rf /var/www/html/leu/database/*
-rm -rf /var/www/html/leu/img/*
-rm -rf /var/www/html/leu/js/*
-rm -rf /var/www/html/leu/lib/*
-rm -rf /var/www/html/leu/php/*
-rm -rf /var/www/html/leu/*.*
-cp -r * /var/www/html/leu
+rm -rf /var/www/html/armory/css/*
+rm -rf /var/www/html/armory/database/*
+rm -rf /var/www/html/armory/img/*
+rm -rf /var/www/html/armory/js/*
+rm -rf /var/www/html/armory/lib/*
+rm -rf /var/www/html/armory/php/*
+rm -rf /var/www/html/armory/*.*
+cp -r * /var/www/html/armory
 cp favicon.ico /var/www/html/
-chown -R www-data:www-data /var/www/html/leu/
+chown -R www-data:www-data /var/www/html/armory/
